c174b52395632533e3eb3fca3298f263faf90afc
noe.cornu <noe.cornu1@gmail.com>
2024-04-06 23:30:36 +0200

    home page bar chart

---
+++ diavenir/lib/DefiPage.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class DefiPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: SizedBox(
+          width: 197,
+          height: 28,
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: 'Bonjour ',
+                  style: TextStyle(
+                    color: Color(0xFF3064E8),
+                    fontSize: 24,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+                TextSpan(
+                  text: 'Robert',
+                  style: TextStyle(
+                    color: Color(0xFF3064E8),
+                    fontSize: 24,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.w700,
+                    height: 0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 100),
+          Center(
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                SvgPicture.asset('assets/svg/defi.svg'),
+                SvgPicture.asset('assets/svg/defi1.svg'),
+              ],
+            ),
+          ),
+          // Other widgets go here...
+        ],
+      ),
+    );
+  }
+}
